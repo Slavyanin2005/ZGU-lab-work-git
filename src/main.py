4640397f4c32a5ac62026577f50d86af9194f4c3
Slavyanin <ostafinskijvadim@gmail.com>
2025-11-10 21:56:29 +0700

    Add addition function

--- src/main.py
+++ src/main.py
@@ -3,10 +3,5 @@ print("Hello, World!")
 def add(a, b):
     return a + b
 
-def multiply(a, b):
-    return a * b
-
-result_add = add(5, 3)
-result_mult = multiply(4, 5)
-print(f"5 + 3 = {result_add}")
-print(f"4 * 5 = {result_mult}")
+result = add(5, 3)
+print(f"5 + 3 = {result}")
