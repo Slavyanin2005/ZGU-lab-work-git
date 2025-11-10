3ef07dd624f06124636661982650913e1bb7b177
Slavyanin <ostafinskijvadim@gmail.com>
2025-11-10 14:39:05 +0700

    Change greeting to Best University message

--- src/main.py
+++ src/main.py
@@ -1,12 +1,7 @@
-print("Hello, world!")
+print("Hello ZGU - Best University!")
 
 def add(a, b):
     return a + b
 
 def multiply(a, b):
     return a * b
-
-result_add = add(5, 3)
-result_mult = multiply(4, 5)
-print(f"5 + 3 = {result_add}")
-print(f"4 * 5 = {result_mult}")
